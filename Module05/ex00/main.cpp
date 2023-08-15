d0138aee78783a9f8dcaffb5135fc514d9671113
perly <aittaleb108@gmail.com>
2023-08-15 17:46:07 +0100

    md05 ex00

---
+++ Module05/ex00/main.cpp
@@ -0,0 +1,40 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    std::cout << "------------test 1----------" << std::endl;
+    try
+    {
+        Bureaucrat a("othmane", 149);
+        std::cout << a;
+        a.decrementGrade();
+        a.decrementGrade();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    // return 0;
+    std::cout << "------------test 2----------" << std::endl;
+    try
+    {
+        Bureaucrat b("said", 2);
+        std::cout << b;
+        b.incrementGrade();
+        b.incrementGrade();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "------------test 2----------" << std::endl;
+    try
+    {
+        Bureaucrat b("name3", 156);
+        std::cout << b;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
