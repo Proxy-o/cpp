b3b4caf1a24822df205c15ebbe6d995a66d38dcc
perly <aittaleb108@gmail.com>
2023-08-20 15:00:18 +0100

    md05 fix add a message when exec done

--- Module05/ex02/Bureaucrat.cpp
+++ Module05/ex02/Bureaucrat.cpp
@@ -100,6 +100,7 @@ void Bureaucrat::executeForm(AForm const &form)
     try
     {
         form.execute(*this);
+        std::cout << this->getName() << " executed " << form.getName() << std::endl;
     }
     catch (std::exception &e)
     {
