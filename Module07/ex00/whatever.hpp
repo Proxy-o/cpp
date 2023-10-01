51637f886ccf64b2095291fc61e9c9cab9da8bdd
proxy <aittaleb108@gmail.com>
2023-10-01 15:21:26 +0100

    md07 ex00

---
+++ Module07/ex00/whatever.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 14:58:21 by otait-ta          #+#    #+#             */
+/*   Updated: 2023/10/01 15:13:12 by otait-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
